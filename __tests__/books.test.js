d41b64dd083752f34fd755e1037e2e7e56af8198
Dave Fisher <dave.j.fish@gmail.com>
2022-08-19 14:39:14 -0700

    paths for getting all books, one book, all authors, one author, adding book or author running

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -5,8 +5,9 @@ const db = require('../lib/models');
 
 describe('books routes', () => {
   beforeEach(async () => {
-    // await db.sequelize.sync({ force: true });
+    await db.sequelize.sync({ force: true });
     try {
+
       await db.books.bulkCreate([
         {
           title: 'super book',
@@ -33,6 +34,72 @@ describe('books routes', () => {
           updatedAt: new Date(),
         },
       ]);
+
+      await db.authors.bulkCreate('authors', [
+        {
+          first_name: 'bill',
+          last_name: 'preston',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'ted',
+          last_name: 'logan',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'dave',
+          last_name: 'fisher',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+      
+      await db.books_authors.bulkCreate('books_authors', [
+        {
+          book_id: '1',
+          author_id: '3',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '2',
+          author_id: '1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '2',
+          author_id: '2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '3',
+          author_id: '1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '3',
+          author_id: '2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '3',
+          author_id: '1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          book_id: '4',
+          author_id: '2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e);
@@ -53,6 +120,28 @@ describe('books routes', () => {
       genre: 'funny',
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
+    });
+  });
+
+  it('#GET /api/v1/books/:id should return a book with nested authors', async () => {
+    const response = await request(app).get('/api/v1/books/2');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: 2,
+      title: 'big book',
+      genre: 'serious',
+      authors: [
+        {
+          id: 2,
+          first_name: 'bill',
+          last_name: 'preston',
+        },
+        {
+          id: 2,
+          first_name: 'ted',
+          last_name: 'logan'
+        },
+      ]
     });
   });
 });
